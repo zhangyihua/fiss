5a194803477d11da424706cb7c392bf0d1ceefb7
zhangyihua <shanzimr@163.com>
2016-03-15 12:04:03 +0800

    解决冲突

--- modules/config.js
+++ modules/config.js
@@ -4,14 +4,23 @@
 
 // 初始化脚手架
 function initScaffold(fis) {
-	fis.set('scaffold.namespace', 'fiss-scaffold');
-	fis.set('scaffold.type', 'github');
+    fis.set('scaffold.namespace', 'fiss-scaffold');
+    fis.set('scaffold.type', 'github');
+}
+
+
+//fis.require.prefixes 增加fiss
+function modifyRequirePrefix(fis) {
+    //default fis.require.prefixes = ['fis3', 'fis'];
+    fis.require.prefixes = ['fiss', 'fis3', 'fis'];
 }
 
 var config = {
-	init: function(fis) {
-		initScaffold(fis);
-	}
+    init: function(fis) {
+        initScaffold(fis);
+        modifyRequirePrefix(fis);
+    }
 }
+
 
 module.exports = config;
